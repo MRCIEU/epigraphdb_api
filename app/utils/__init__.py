3ff72c7abec476890b8ee9597a9f44799001e7f9
YiLiu6240 <yi.liu6240@gmail.com>
2020-04-26 20:58:49 +0100

    Refine testing of case studies

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 import pandas as pd
+from loguru import logger
 
 
 def cypher_fuzzify(text: Optional[str]) -> Optional[str]:
@@ -24,18 +25,24 @@ def df_coerce(
     This is primarily for unit testing dataframes.
     """
 
+    # Remove missing for correct inference of column types
+    df = df.dropna()
     bool_number_cols = df.select_dtypes(
         include=["bool", "number"]
     ).columns.tolist()
     string_type_map = df.applymap(lambda x: isinstance(x, str)).all(0)
     string_cols = [i for i in string_type_map.index if string_type_map[i]]
     cols = list(set(bool_number_cols).union(set(string_cols)))
-    df = df.sort_values(by=cols)
+    logger.info(f"sort by cols: {cols}")
     if list_columns is not None:
         for col in list_columns:
             df[col] = df[col].apply(
                 lambda x: sorted(x) if x is not None else x
             )
+    df = (
+        df
+        .sort_values(by=cols, kind="mergesort")
+        .reset_index(drop=True)
+    )
 
-    res_df = df.reset_index(drop=True)
-    return res_df
+    return df
