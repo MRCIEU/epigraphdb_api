aacc8d1d32bd787f135fc747443b83fb6c117dc3
YiLiu6240 <yi.liu6240@gmail.com>
2020-04-30 10:57:30 +0100

    Be more verbose about df_coerce

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -18,6 +18,7 @@ def df_coerce(
     df: pd.DataFrame,
     order_by: Optional[List[str]] = None,
     list_columns: Optional[List[str]] = None,
+    verbose: bool = True,
 ) -> pd.DataFrame:
     """Coerce df by:
 
@@ -38,12 +39,15 @@ def df_coerce(
         cols = list(set(bool_number_cols).union(set(string_cols)))
     else:
         cols = order_by
-    logger.info(f"sort by cols: {cols}")
+    if verbose:
+        logger.info(f"sort by cols: {cols}")
     if list_columns is not None:
         for col in list_columns:
             df[col] = df[col].apply(
                 lambda x: sorted(x) if x is not None else x
             )
     df = df.sort_values(by=cols, kind="mergesort").reset_index(drop=True)
+    if verbose:
+        logger.info(f"df.head(10): \n{df.head(10)}")
 
     return df
