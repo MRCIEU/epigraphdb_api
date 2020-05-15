72b85992c32f9fbe5f5bdcc0ce29bd05c8e447f7
YiLiu6240 <yi.liu6240@gmail.com>
2020-05-15 14:48:41 +0100

    Fix pandas slicing

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -43,7 +43,7 @@ def df_coerce(
         logger.info(f"sort by cols: {cols}")
     if list_columns is not None:
         for col in list_columns:
-            df[col] = df[col].apply(
+            df.loc[:, col] = df[col].apply(
                 lambda x: sorted(x) if x is not None else x
             )
     df = df.sort_values(by=cols, kind="mergesort").reset_index(drop=True)
