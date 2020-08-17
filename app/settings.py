911cd315efd1881f83e2fdb40e4e8f4874ec6379
YiLiu6240 <yi.liu6240@gmail.com>
2020-08-17 10:48:08 +0100

    Simplify names

--- app/settings.py
+++ app/settings.py
@@ -4,16 +4,16 @@ Control and configure global settings.
 from pathlib import Path
 
 from app.utils.database import Neo4jDB
-from epigraphdb_common_utils.api_env_configs import configs as api_env_configs
+from epigraphdb_common_utils.api_env_configs import configs as env_configs
 
 # General config
 epigraphdb_version = "0.3"
 api_version = "0.3"
-api_private_access = api_env_configs["api_private_access"]
+api_private_access = env_configs["api_private_access"]
 if api_private_access:
     api_version = f"{api_version}-private"
 
-api_key = api_env_configs["api_key"]
+api_key = env_configs["api_key"]
 
 # directories
 log_dir = Path("/tmp/epigraphdb_api/logs")
@@ -23,18 +23,18 @@ cache_dir.mkdir(parents=True, exist_ok=True)
 
 # EpiGraphDB database
 epigraphdb = Neo4jDB(
-    hostname=api_env_configs["epigraphdb_server"],
-    bolt_port=api_env_configs["epigraphdb_port"],
-    user=api_env_configs["epigraphdb_user"],
-    password=api_env_configs["epigraphdb_passwd"],
-    version=api_env_configs["epigraphdb_db_version"],
+    hostname=env_configs["epigraphdb_server"],
+    bolt_port=env_configs["epigraphdb_port"],
+    user=env_configs["epigraphdb_user"],
+    password=env_configs["epigraphdb_passwd"],
+    version=env_configs["epigraphdb_db_version"],
 )
 
 # PQTL database
 pqtl = Neo4jDB(
-    hostname=api_env_configs["pqtl_server"],
-    bolt_port=api_env_configs["pqtl_port"],
-    user=api_env_configs["pqtl_user"],
-    password=api_env_configs["pqtl_passwd"],
-    version=api_env_configs["pqtl_db_version"],
+    hostname=env_configs["pqtl_server"],
+    bolt_port=env_configs["pqtl_port"],
+    user=env_configs["pqtl_user"],
+    password=env_configs["pqtl_passwd"],
+    version=env_configs["pqtl_db_version"],
 )
