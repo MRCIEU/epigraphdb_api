bdbe3fdfe553e66d6ff0e0f9e9cc7e8984d411e4
YiLiu6240 <yi.liu6240@gmail.com>
2020-09-17 16:22:42 +0100

    Update how meta entities are rendered

--- scripts/render_schema.py
+++ scripts/render_schema.py
@@ -1,50 +1,51 @@
-from pprint import pprint
-
-from app.models.schema_meta_nodes import meta_node_schema
-from app.models.schema_meta_rels import meta_path_schema, meta_rel_schema
-
-
-def format_schema(orig_schema):
-    # keys = orig_schema.keys()
-    # schema = {
-    #     "properties": {
-    #         key: {"type": value["type"]}
-    #         for key, value in orig_schema["properties"].items()
-    #     },
-    #     "required": orig_schema["required"] if "required" in keys else {},
-    # }
-    schema = orig_schema
-    return schema
+from epigraphdb_common_utils.epigraphdb_data_dicts import (
+    meta_nodes_dict_sanitised,
+    meta_rels_dict_sanitised,
+)
 
 
 def rmd_render_meta_nodes():
-    for name, model in meta_node_schema.items():
-        schema = format_schema(model.schema())
+    for name, data in meta_nodes_dict_sanitised.items():
         print(f"## `{name}`\n")
 
-        if "description" in list(model.schema().keys()):
-            print("**Description**:\n")
-            print(model.schema()["description"], "\n")
+        if data["doc"] is not None:
+            print("> ", data["doc"], "\n")
 
-        print("**Schema**:\n")
-        print("```")
-        pprint(schema)
-        print("```")
+        if data["id"] is not None:
+            print("**`id` property**: `{id}`\n".format(id=data["id"],))
+        if data["name"] is not None:
+            print(
+                "**`label` property**: `{label}`\n".format(label=data["name"],)
+            )
+
+        if data["properties"] is not None:
+            print("**Properties**:\n")
+            render_properties(data["properties"])
+            print("\n")
 
 
 def rmd_render_meta_rels():
-    for name, model in meta_rel_schema.items():
-        schema = format_schema(model.schema())
-        path = meta_path_schema[name]
+    for name, data in meta_rels_dict_sanitised.items():
         print(f"## `{name}`\n")
 
-        if "description" in list(model.schema().keys()):
-            print("**Description**:\n")
-            print(model.schema()["description"], "\n")
+        if data["doc"] is not None:
+            print("> ", data["doc"], "\n")
 
-        print(f"**Path**: `({path[0]})-[{name}]->({path[1]})`\n")
+        print(
+            "**Path**: `({source})-[{name}]->({target})`\n".format(
+                source=data["source"], target=data["target"], name=name
+            )
+        )
 
-        print("**Schema**:\n")
-        print("```")
-        pprint(schema)
-        print("```")
+        if data["properties"] is not None:
+            print("**Properties**:\n")
+            render_properties(data["properties"])
+            print("\n")
+
+
+def render_properties(property):
+    for key, value in property.items():
+        if value is None:
+            print("- `{prop}`\n".format(prop=key))
+        else:
+            print("- `{prop}`: {doc}\n".format(prop=key, doc=value))
