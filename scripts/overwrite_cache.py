3e090e5ce77ff2f2f1117f24d0f6a8e19409ea28
YiLiu6240 <yi.liu6240@gmail.com>
2021-03-21 13:22:27 +0000

    Allow graphviz plot to be regen in overwrite_cache

--- scripts/overwrite_cache.py
+++ scripts/overwrite_cache.py
@@ -11,7 +11,7 @@ def main():
     logger.info("Overwrite schema cache.")
     client.get(
         "/meta/schema",
-        params={"graphviz": False, "plot": False, "overwrite": True},
+        params={"graphviz": True, "plot": True, "overwrite": True},
     )
 
     logger.info("Done.")
