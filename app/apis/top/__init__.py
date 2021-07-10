f33f43bd6f543d3bb9d7023c311493dfb78a9034
YiLiu6240 <yi.liu6240@gmail.com>
2021-07-10 13:05:27 +0100

    Check dependencies to GET /ping

--- app/apis/top/__init__.py
+++ app/apis/top/__init__.py
@@ -1,9 +1,21 @@
+import requests
 from fastapi import APIRouter
+
+from app.settings import epigraphdb, neural_url, pqtl, public_graph
 
 router = APIRouter()
 
 
 @router.get("/ping", response_model=bool)
-def get_top_ping():
+def get_top_ping(dependencies: bool = True) -> bool:
     """Test that you are connected to the API."""
-    return True
+    if not dependencies:
+        return True
+    else:
+        epigraphdb_ok = epigraphdb.check_connection()
+        pqtl_ok = pqtl.check_connection()
+        public_graph_ok = public_graph.check_connection()
+        neural_ok = requests.get(f"{neural_url}/ping").ok
+        all = [epigraphdb_ok, pqtl_ok, public_graph_ok, neural_ok]
+        all_ok = sum(all) == len(all)
+        return all_ok
